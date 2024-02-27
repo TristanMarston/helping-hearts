e27d36c9345f3aed9d0e9090d1d79d81c608e54d
Tristan Marston <tristan@ericmarston.com>
2024-02-27 07:37:13 -0800

    signup page small fix

--- src/app/signup/_components/Signup.tsx
+++ src/app/signup/_components/Signup.tsx
@@ -26,40 +26,35 @@ function SignupContent() {
     }, [searchParams]);
 
     return (
-        <div className='w-full flex justify-center'>
-            <div className='w-full max-w-[1280px] flex flex-col mx-6 my-8'>
-                <h1 className={`${jua.className} text-4xl`}>Register Today!</h1>
-                <Suspense fallback={<Fallback />}>
-                    <div className={`${nunitoLight.className} flex flex-col gap-1 mt-1`}>
-                        <p>
-                            You are currently signing up {showVolunteer ? 'as a' : 'for a'} <b>{showVolunteer ? 'volunteer' : 'participant'}</b>.
-                        </p>
-                        <p>
-                            {showVolunteer
-                                ? 'Please enter the following information about yourself to volunteer.'
-                                : 'Please enter the following information for the child participant(s) and corresponding emergency contact information (parent or guardian).'}
-                        </p>
-                    </div>
-                    <div className='w-full h-10 rounded-full grid grid-cols-2 mt-4 bg-background shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]'>
-                        <Link
-                            className={`${nunitoBold.className} ${showVolunteer ? 'bg-background text-black' : 'bg-primary text-white'} rounded-full flex justify-center items-center`}
-                            onClick={() => setShowVolunteer((prev) => !prev)}
-                            href='/signup?isVolunteer=false'
-                        >
-                            Participant(s)
-                        </Link>
-                        <Link
-                            className={`${nunitoBold.className} ${!showVolunteer ? 'bg-background text-black' : 'bg-primary text-white'} rounded-full flex justify-center items-center`}
-                            onClick={() => setShowVolunteer((prev) => !prev)}
-                            href='/signup?isVolunteer=true'
-                        >
-                            Volunteer
-                        </Link>
-                    </div>
-                    {!showVolunteer ? <ParticipantSignUp /> : <VolunteerSignUp />}
-                </Suspense>
+        <Suspense fallback={<Fallback />}>
+            <div className={`${nunitoLight.className} flex flex-col gap-1 mt-1`}>
+                <p>
+                    You are currently signing up {showVolunteer ? 'as a' : 'for a'} <b>{showVolunteer ? 'volunteer' : 'participant'}</b>.
+                </p>
+                <p>
+                    {showVolunteer
+                        ? 'Please enter the following information about yourself to volunteer.'
+                        : 'Please enter the following information for the child participant(s) and corresponding emergency contact information (parent or guardian).'}
+                </p>
             </div>
-        </div>
+            <div className='w-full h-10 rounded-full grid grid-cols-2 mt-4 bg-background shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]'>
+                <Link
+                    className={`${nunitoBold.className} ${showVolunteer ? 'bg-background text-black' : 'bg-primary text-white'} rounded-full flex justify-center items-center`}
+                    onClick={() => setShowVolunteer((prev) => !prev)}
+                    href='/signup?isVolunteer=false'
+                >
+                    Participant(s)
+                </Link>
+                <Link
+                    className={`${nunitoBold.className} ${!showVolunteer ? 'bg-background text-black' : 'bg-primary text-white'} rounded-full flex justify-center items-center`}
+                    onClick={() => setShowVolunteer((prev) => !prev)}
+                    href='/signup?isVolunteer=true'
+                >
+                    Volunteer
+                </Link>
+            </div>
+            {!showVolunteer ? <ParticipantSignUp /> : <VolunteerSignUp />}
+        </Suspense>
     );
 }
 
