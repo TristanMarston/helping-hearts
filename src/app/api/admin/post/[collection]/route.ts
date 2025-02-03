3120ca7a3331bb881270806bcf335d49a51476ee
TristanMarston <tristan@ericmarston.com>
2025-02-02 21:04:34 -0800

    server-side route fix #2

--- src/app/api/admin/post/[collection]/route.ts
+++ src/app/api/admin/post/[collection]/route.ts
@@ -1,17 +1,22 @@
 import axios from 'axios';
 import { NextResponse } from 'next/server';
 
-export async function POST(request: Request, { params }: { params: { collection: string } }) {
-    const { collection } = params;
+export async function POST(request: Request, context: { params: { collection?: string } }) {
+    if (!context.params || !context.params.collection) {
+        return NextResponse.json({ message: 'Please provide collection.' }, { status: 400 });
+    }
+
+    const collection = context.params.collection;
     const data = await request.json();
 
-    if (Object.keys(data).length === 0) return NextResponse.json({ message: 'Please provide body.' }, { status: 500 });
-    if (collection === null || collection.length === 0) return NextResponse.json({ message: 'Please provide collection.' }, { status: 500 });
+    if (Object.keys(data).length === 0) {
+        return NextResponse.json({ message: 'Please provide body.' }, { status: 400 });
+    }
 
     try {
         const response = await axios.post(`${process.env.API_URL}/${collection}`, data, {
             headers: {
-                'x-api-key': process.env.API_KEY,
+                'x-api-key': process.env.API_KEY!,
             },
         });
 
